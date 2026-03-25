574429a43b64308da4206112dff3f5c9869eaec6
yuzh1363 <yuzh0806@gmail.com>
2026-03-25 20:31:48 +0800

    Build Vue project in subfolder

--- My_Note/vite.config.js
+++ My_Note/vite.config.js
@@ -4,4 +4,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/UIUX03-20/My_Note/',
+  build: {
+    outDir: './', // 讓打包後的 index.html 直接產出在 My_Note 資料夾中
+  }
 })
